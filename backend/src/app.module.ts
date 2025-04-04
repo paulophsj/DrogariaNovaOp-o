f9a1da6b0409ebd18528da4c94135f4e76499d28
paulophsj <snoopyphs@gmail.com>
2025-04-04 20:29:43 -0300

    Add .env file and updating database archives

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,15 +6,17 @@ import { Product } from './entities/product.entity';
 import { ProductModule } from './product/product.module';
 import { ComprasModule } from './compras/compras.module';
 import { Compras } from './entities/compras.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root',
-      password: 'root',
+      username: process.env.DB_USERNAME ? process.env.DB_USERNAME : 'root',
+      password: process.env.DB_PASSWORD ? process.env.DB_PASSWORD : 'root',
       database: 'drogaria',
       entities: [Product, Compras],
       synchronize: true,
--- backend/src/database.provider.ts
+++ backend/src/database.provider.ts
@@ -4,8 +4,8 @@ export const createDatabase = async () => {
   const connection = await createConnection({
     host: 'localhost',
     port: 3306,
-    user: 'root',
-    password: 'root',
+    user: process.env.DB_USERNAME ? process.env.DB_USERNAME : 'root',
+    password: process.env.DB_PASSWORD ? process.env.DB_PASSWORD : 'root',
   });
 
   await connection.query(`CREATE DATABASE IF NOT EXISTS drogaria`);
